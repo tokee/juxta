d589521a8b58d6c326677c75b966fc3755244949
Toke Eskildsen <te@ekot.dk>
2017-01-29 00:17:10 +0100

    Added notes on paging limitations and suggestion for workaround

--- demo_kb.sh
+++ demo_kb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Creates collage of public available images from kb.dk
+# API described on https://github.com/Det-Kongelige-Bibliotek/access-digital-objects
 
 : ${MAX_IMAGES:=1000}
 : ${BROWSE_URL:="http://www.kb.dk/images/billed/2010/okt/billeder"}
@@ -55,6 +56,9 @@ download_collection() {
     while [ $(( (PAGE-1)*PAGE_SIZE )) -lt $MAX_IMAGES ]; do
         local URL="${SEARCH_URL_PREFIX}page=${PAGE}&subject=${SUBJECT_ID}"
         echo "  - Fetching page ${PAGE}: $URL"
+        # TODO: Seems to be limited to 400 total entries
+        # http://www.kb.dk/maps/kortsa/2012/jul/kortatlas/subject233/da/?orderBy=notBefore&page=2
+        # Seems to allow for paging all the way to the end of the search result
         curl -s -m 60 "$URL" | xmllint --format - > $T
         local HITS=$( cat $T | grep totalResults | sed 's/.\+>\([0-9]\+\)<.*/\1/' )
         IFS=$'\n'
@@ -66,6 +70,9 @@ download_collection() {
             local IMAGE_SHORT=`basename "$IMAGE_URL" | sed 's/ /_/g'`
             if [ ! -s downloads/$COLLECTION/$IMAGE_SHORT ]; then
                 echo "    - Downloading $IMAGE_SHORT"
+                # TODO: Fetch full image with
+                # http://kb-images.kb.dk/online_master_arkiv_6/non-archival/Images/BILLED/2008/Billede/dk_eksp_album_191/kbb_alb_2_191_friis_011/full/full/0/native.jpg
+                # https://github.com/Det-Kongelige-Bibliotek/access-digital-objects/blob/master/image-delivery.md
                 curl -s -m 60 "$IMAGE_URL" > downloads/$COLLECTION/$IMAGE_SHORT
                 if [ ! -s downloads/$COLLECTION/$IMAGE_SHORT ]; then
                     >&2 echo "Error: Unable do download $IMAGE_URL"
