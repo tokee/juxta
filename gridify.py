989ca9f9be8e4c2ee859b644e825d196a9f25744
Toke Eskildsen <te@ekot.dk>
2019-11-05 22:04:07 +0100

    Add first real take

--- gridify.py
+++ gridify.py
@@ -1,16 +1,21 @@
 import rasterfairy
 import json
 import numpy as np
+import math
+
+# Expects output from tSNE-images.py and runs rasterfairy on the input,
+# writing the image paths layed out to a grid as the output
 
 with open('/home/te/projects/ml4a-ofx/points.json') as json_file:
     data = json.load(json_file)
 
-for j in data:
-    print(j['point'])
+arr = []
+for tup in data:
+    point = tup['point']
+    arr.append([point[0], point[1]])
+#    arr.append([math.floor(point[0]*100000000), math.floor(point[1]*100000000)])
 
-    print("**************************************")
-    
-full_data = np.asarray( data )
+#full_data = np.asarray( data )
 #  {'path': '/home/te/projects/ml4a-ofx/images/20190902-0853_4.jpg', 'point': [0.1758463829755783, 0.3165808618068695]}
 #for entry in full_data:
 #    print (entry)
@@ -18,18 +23,38 @@ full_data = np.asarray( data )
 
 #print(full_data)
 
-arr = np.asarray( [[1000, 2000], [2, 30], [8, 100], [300, 2]])
-print(arr)
+#arr = np.asarray( [[1000, 2000], [2, 30], [8, 100], [300, 2]])
+#print(arr)
 tsne = np.asarray(arr)
-print(tsne)
+#print(tsne)
 
-# nx * ny = 1000, the number of images
-nx = 2
-ny = 2
+nx = 15
+ny = 21
 
-# assign to grid
+# 16x20 hangs forever!?
+#nx = 16
+#ny = 20
+
 #grid = rasterfairy.transformPointCloud2D(tsne)
-grid = rasterfairy.transformPointCloud2D(tsne, target=(nx, ny))
-print(grid)
+print("Calling rasterfairy on " + str(len(arr)) + " coordinates")
+gridAssignment = rasterfairy.transformPointCloud2D(tsne, target=(nx, ny))
+grid, gridShape = gridAssignment
+
+out_grid = []
+for i, dat in enumerate(data):
+    gridX, gridY = grid[i]
+    out_grid.append({'gx': int(gridX), 'gy': int(gridY), 'path': dat['path']})
+#    print(dat['path'] + " gx:" + str(int(gridX)) + ", gy:" + str(int(gridY)))
+
+#out_grid = out_grid.sort(key = lambda obj: obj['gx'], obj['gy'])
+
+# We sort by secondary first - Python sort is stable; it does not change order on equal keys
+out_grid.sort(key = lambda obj: obj['gy'])
+out_grid.sort(key = lambda obj: obj['gx'])
+
+for element in out_grid:
+    print(element['path'])
+
+#print(out_grid)
 
 
