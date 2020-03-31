8d040a59aff69ca80dc1143c24b28a474aeb91e4
Toke Eskildsen <te@ekot.dk>
2020-03-31 15:18:33 +0200

    Add support for metadata

--- tensorflow_sort.sh
+++ tensorflow_sort.sh
@@ -70,12 +70,13 @@ check_parameters() {
 
     # downloads/subject3795/516937.jpg|http://www.kb.dk/images/billed/2010/okt/billeder/object171949/da/§Holbæk. Parti fra Havnen§§CC BY-NC-ND
 
-    local UNIQUE_COUNT=$(sed -e 's%|.*%%' -e 's%.*/%%' < "$IN" | LC_ALL=C sort | LC_ALL=c uniq | wc -l)
-    if [[ "$IN_COUNT" -ne "$UNIQUE_COUNT" ]]; then
-        >&2 echo "Error: The input $IN ($IN_COUNT images) contained duplicate (de-duplicate $UNIQUE_COUNT images) file names: [$(sed -e 's%|.*%%' -e 's%.*/%%' < "$IN" | LC_ALL=C sort | LC_ALL=c uniq -c | grep -v " 1 " | sed 's/ *[0-9]\+ //' | tr '\n' ' ')]"
-        exit 13
+    # Uniqueness is not a requirement after switch to new Python script
+#    local UNIQUE_COUNT=$(sed -e 's%|.*%%' -e 's%.*/%%' < "$IN" | LC_ALL=C sort | LC_ALL=c uniq | wc -l)
+#    if [[ "$IN_COUNT" -ne "$UNIQUE_COUNT" ]]; then
+#        >&2 echo "Error: The input $IN ($IN_COUNT images) contained duplicate (de-duplicate $UNIQUE_COUNT images) file names: [$(sed -e 's%|.*%%' -e 's%.*/%%' < "$IN" | LC_ALL=C sort | LC_ALL=c uniq -c | grep -v " 1 " | sed 's/ *[0-9]\+ //' | tr '\n' ' ')]"
+#        exit 13
 
-    fi
+#    fi
     if [[ -z "$OUT" ]]; then
         >&2 echo "Error: No out_imagelist.dat specified"
         usage 14
@@ -165,10 +166,13 @@ perform_analysis() {
     echo "- Similarity sorting and gridifying ${IN} with --grid_width=${RAW_IMAGE_COLS} --grid_height=${RAW_IMAGE_ROWS}"
     echo "- NOTE: RasterFairy hangs on some grid layouts (the cause is not known). If nothing happens after this line, try re-running with RAW_IMAGE_COLS=$((RAW_IMAGE_COLS-1)) or $((RAW_IMAGE_COLS+1))"
 
+    # Strip metadata
+    T_ONLY_PATHS=$(mktemp)
+    cut -d'|' -f1 < "$IN" > "$T_ONLY_PATHS"
     T_GRID=$(mktemp)
-    python3 ${SCRIPT_HOME}/imagenet_tsne_rasterfairy.py --images ${IN} --grid_width=${RAW_IMAGE_COLS} --grid_height=${RAW_IMAGE_ROWS} --output=${OUT_FULL} | grep "Stored result in .* grid dimensions.*" | tee "$T_GRID"
+    python3 ${SCRIPT_HOME}/imagenet_tsne_rasterfairy.py --images ${T_ONLY_PATHS} --grid_width=${RAW_IMAGE_COLS} --grid_height=${RAW_IMAGE_ROWS} --output=${OUT_FULL} | tee "$T_GRID"
     GRID=$(grep "Stored result" "$T_GRID" | grep -o " [0-9]*x[0-9]*" | tr -d \  )
-    rm "$T_GRID"
+    rm "$T_GRID" "$T_ONLY_PATHS"
 
     GX=$(cut -dx -f1 <<< "$GRID")
     GY=$(cut -dx -f2 <<< "$GRID")
@@ -179,45 +183,29 @@ convert_to_list() {
     echo "RAW_IMAGE_COLS=$GX RAW_IMAGE_ROWS=$GY ./juxta.sh \"$OUT\" \"$RENDER\""
 }
 
-# The OUT_FINAL contains the images in the correct order, but with wrong paths
-# This methods assigns the paths from IN to OUT_FINAL, preserving the order
-fix_paths() {
-    pushd $CACHE_FOLDER > /dev/null
-    # https://stackoverflow.com/questions/407523/escape-a-string-for-a-sed-replace-pattern#2705678
-    local CFULL=$(sed 's/[]\/$*.^[]/\\&/g' <<< "$(pwd)/" )
-    popd > /dev/null
-    sed -i "s/$CFULL//" "$OUT_FINAL"
-    # OUT_FINAL now holds filenames only in tsne order
-
-    T1=$(mktemp)
+# The OUT contains the images in the correct order, but without metadata
+# This methods assigns the metadata from IN to OUT, preserving the order
+add_metadata() {
+    local T1=$(mktemp)
     local COUNTER=0
     while read -r IMG; do
         echo "$IMG"$'\t'"$COUNTER" >> "$T1"
         COUNTER=$(( COUNTER + 1 ))
-    done <"$OUT_FINAL"
-    # T1 now holds filenames counter in tsne order
+    done <"$OUT"
+    # T1 now holds [path counter] in tsne order
     
     T1B=$(mktemp)
     LC_ALL=c sort < "$T1" > "$T1B"
-    # T1 now holds filenames counter in filenames order
-    
+    # T1 now holds [path counter] in path order
+
     T2=$(mktemp)
-    sed 's%^\([^|]*\)/\([^/]*\)\([|].*\)\?$%\2\t\1\t\3%' < "$IN" | LC_ALL=c sort > "$T2"
-    # T2 now holds filename path meta in filenames order
+    sed 's%^\([^|]*\)\([|].*\)\?$%\1\t\2%' < "$IN" | LC_ALL=c sort > "$T2"
+    # T2 now holds [path meta] in path order
     
-    paste "$T1B" "$T2" | sed 's/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/\2\t\4\/\3\5/' | sort -n | sed 's/^[^\t]*\t//' > "$T1"
-    # T1 held counter path/filename/meta in tsne (aka counter) order, and had the counter-part removed
+    paste "$T1B" "$T2" | sed 's/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/\2\t\3\4/' | sort -n | sed 's/^[^\t]*\t//' > "$T1"
     
-    mv "$T1" "$OUT_FINAL"
+    mv "$T1" "$OUT"
     rm "$T1B" "$T2"
-    cp "$OUT_FINAL" "$OUT"
-    echo "- Fixed paths for $OUT_FINAL and copied to $OUT"
-
-    echo "Finihed sorting. Sample juxta call:"
-
-    local RENDER=$(basename "${OUT_FINAL%.*}")
-
-    echo "RAW_IMAGE_COLS=$GX RAW_IMAGE_ROWS=$GY ./juxta.sh \"$OUT_FINAL\" \"$RENDER\""
 }
 
 
@@ -229,4 +217,5 @@ check_parameters "$@"
 ensure_environment
 perform_analysis
 convert_to_list
+add_metadata
 #fix_paths
