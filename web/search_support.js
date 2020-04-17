0fc7393397ad75ab74ab34d1861740d3e39510bc
Toke Eskildsen <te@ekot.dk>
2020-04-17 17:23:09 +0200

    Add first demo of overlays

--- web/search_support.js
+++ web/search_support.js
@@ -10,6 +10,96 @@ searchConfig = {
     maxMatches: 1000,
 }
 
+// https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
+
+
+var svg = null;
+var diffusor = null;
+var svgString = '';
+var homeBounds = null;
+function createSVGOverlay() {
+    homeBounds = myDragon.viewport.getHomeBounds();
+    /* Must be before the svg so that it is positioned underneath */
+    diffusor = document.createElement("div");
+    diffusor.id = "diffusor-overlay";
+    myDragon.addOverlay({
+        element: diffusor,
+        location: homeBounds
+    });
+
+    svg = document.createElement("div");
+    svg.id = "svg-overlay";
+    myDragon.addOverlay({
+        element: svg,
+//        location: new OpenSeadragon.Rect(0, 0, 1.05, 1)
+        location: homeBounds
+    });
+    svgString = '';
+}
+createSVGOverlay();
+
+function clearSVGOverlay() {
+    if (diffusor) {
+        svgString = '';
+        updateSVGOverlay('');
+        diffusor.style.opacity = 0.0;
+    } else {
+        createSVGOverlay();
+    }
+}
+console.log("HomeBounds: " + JSON.stringify(myDragon.viewport.getHomeBounds()));
+
+var jprops = overlays.jprops;
+console.log(JSON.stringify(jprops));
+function updateSVGOverlay(svgXML) {
+
+    svgString += svgXML;
+    svg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" style="position:absolute;z-index:10;margin:0;padding:0;top:0;left:0;width:100%;height:100%" viewBox="{0} {1} {2} {3}">{4}</svg>'.format(homeBounds.x, homeBounds.y, homeBounds.width, homeBounds.height, svgString);
+
+    //    svg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" style="position:absolute;z-index:10;margin:0;padding:0;top:0;left:0;width:100%;height:100%" viewBox="{0} {1} {2} {3}">{4}</svg>'.format(0, 0, 1, 1, svgString);
+    
+//    svg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" style="position:absolute;z-index:10;margin:0;padding:0;top:0;left:0;width:100%;height:100%" viewBox="0 0 ' + jprops.colCount + ' ' + jprops.rowCount + '">' + svgString + '</svg>';
+    diffusor.style.opacity = 0.8;
+}
+
+function addBox(boxID) {
+   
+    x = boxID % jprops.colCount;
+    y = Math.floor(boxID / jprops.colCount);
+    boxW = 1 / jprops.colCount;
+    boxH = 1 / jprops.rowCount;
+    lineWidth = 0.01 * 1 / Math.min(jprops.colCount, jprops.rowCount);
+ 
+   xFactor = homeBounds.width+homeBounds.x;
+    yFactor = homeBounds.height+homeBounds.y;
+    
+//    console.log("box={0], x={1}, y={2}, xFactor={3}, yFactor={4}".format(boxID, x, y, xFactor, yFactor));
+    //boxW *= xFactor;
+    //boxH *= yFactor;
+    x = x / jprops.colCount;
+    y = y / jprops.rowCount * 0.923;
+    boxH *= 0.923;
+    
+    updateSVGOverlay('<rect x="{0}" y="{1}" width="{2}" height="{3}" style="fill:transparent;stroke-width:{4};stroke:{5}" />\n'.format(x, y, boxW, boxH, lineWidth, "#8888ff"));
+}
+for (var i = 0 ; i < 15 ; i++) {
+    addBox(i*14);
+    addBox(i*14+3);
+    addBox(i*14+6);
+    addBox(i*14+9);
+}
+
+//updateSVGOverlay('<rect x="-2.125" y="0" width="4" height="4" style="fill:transparent;stroke-width:0.1;stroke:#ffffff" />\n')
+//updateSVGOverlay('<rect x="-1.46" y="0" width="13" height="19" style="fill:transparent;stroke-width:0.1;stroke:#00ffff" />\n')
+    
 function clearSearch() {
     console.log("Clearing previous search result")
 }
@@ -82,3 +172,4 @@ function enableSearch() {
     }
 }
 enableSearch();
+
